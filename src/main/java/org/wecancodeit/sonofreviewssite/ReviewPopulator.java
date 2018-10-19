978989e8f68332b241989d097f6d6025faeb67b6
Riley <rileyjeanpatrick@gmail.com>
2018-10-19 13:14:52 -0400

    Poplator fully populated

--- src/main/java/org/wecancodeit/sonofreviewssite/ReviewPopulator.java
+++ src/main/java/org/wecancodeit/sonofreviewssite/ReviewPopulator.java
@@ -17,15 +17,19 @@ public class ReviewPopulator implements CommandLineRunner {
 	@Resource
 	ReviewRepository reviewRepo;
 
-	Category category1 = new Category("Sweaters");
-	Category category2 = new Category("Life Choices");
-	Category category3 = new Category("Kids: Which is Best");
-	Category category4 = new Category("Hobbies");
-	Category category5 = new Category("Worst Dad Jokes");
-
 	@Override
 	public void run(String... args) throws Exception {
+		Category category1 = new Category("Sweaters");
+		Category category2 = new Category("Life Choices");
+		Category category3 = new Category("Kids: Which is Best");
+		Category category4 = new Category("Hobbies");
+		Category category5 = new Category("Worst Dad Jokes");
 
+		category1 = categoryRepo.save(category1);
+		category2 = categoryRepo.save(category2);
+		category3 = categoryRepo.save(category3);
+		category4 = categoryRepo.save(category4);
+		category5 = categoryRepo.save(category5);
 	}
 
 }
