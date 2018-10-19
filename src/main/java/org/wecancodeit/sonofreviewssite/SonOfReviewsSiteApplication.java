c54e90399ba2e55c57dc091addc854b54087ec79
maxmraz <max.mraz.11@gmail.com>
2018-10-19 10:39:27 -0400

    initial commit

---
+++ src/main/java/org/wecancodeit/sonofreviewssite/SonOfReviewsSiteApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.sonofreviewssite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonOfReviewsSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonOfReviewsSiteApplication.class, args);
+	}
+}
