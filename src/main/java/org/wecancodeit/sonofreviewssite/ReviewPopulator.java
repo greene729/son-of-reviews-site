eda600bfd12eacec84d11176a11fa8762cf1b9db
Riley <rileyjeanpatrick@gmail.com>
2018-10-21 23:04:01 -0400

    Images Added

--- src/main/java/org/wecancodeit/sonofreviewssite/ReviewPopulator.java
+++ src/main/java/org/wecancodeit/sonofreviewssite/ReviewPopulator.java
@@ -50,19 +50,19 @@ public class ReviewPopulator implements CommandLineRunner {
 		Review sweater1 = new Review("Greg's Christmas Sweater",
 
 				"Never have I been so gifted, as to see Greg's Chrsitmas Sweater. It was the greatest gift one could recieve. I believe his Great Aunt Mable knitted it for him some years back. It is covered in the finest layer of cat hair, from said Great Aunt Mable. He begins wearing this sweater sometime in mid November and does not take it off till the new year. I dare say he bathes in it. Of all his sweaters this is one of my favorites.",
-				"Recommendation: Keep it", "8.7/10", "image", category1);
+				"Recommendation: Keep it", "8.7/10", "christmas.jpg", category1);
 		Review sweater2 = new Review("Greg's College Sweater",
 				"This sweater eminates a kind of naive, hapless earnestness. It doesn't seem to notice how tight it's become, to its benefit. Greg bought this his sophomore year after he finished his cynical phase, and almost lost it in the lake during one of our late-night escapades. The way it smelled for the next week, I wish he had.",
-				"Recommendation: Keep it", "8.7/10", "image", category1);
+				"Recommendation: Keep it", "8.7/10", "football.jpg", category1);
 		Review sweater3 = new Review("Greg's Halloween Sweater",
 				"Greg has made a lot of bad decisions in his life. There was the time share in Nepal, there was the vacation to Indianapolis, but his Halloween Sweater makes it into the cannon of great disasters. It is neither spooky, nor cute. I've never felt the hint of a shiver run down my spine. I've never felt warm, fuzzy, harvest-time feelings either. What purpose does this sweater serve? What is its target audience? I don't think anyone knows.",
-				"Recommendation: Keep it", "4/10", "image", category1);
+				"Recommendation: Keep it", "4/10", "halloween.jpg", category1);
 		Review sweater4 = new Review("Greg's Inappropriately Small Sweater",
 				"Why would you give such an ill-fitting sweater such a high score? I went back and forth on this, but I decided in the end that while it gains no points for fashion, taste, respectability, sense, or general ability of anyone accompanying Greg to maintain their composure, it has its own appeal. When Greg was in high school, he spent a summer abroad in Bratislava, and his little sister decided it might get cold there, so she bought him a wool sweater. As soon as Greg arrived in Czechoslovakia he washed and dried his sweater and shrunk it down to dog size. He felt so bad about it that he continued wearing it anyway, and has since.",
-				"Recommendation: Keep it", "10/10", "image", category1);
+				"Recommendation: Keep it", "10/10", "small.jpg", category1);
 		Review sweater5 = new Review("Greg's Kind of Classy Sweater",
 				"rThis one is an honest argyle Greg found for 10% off at JC Penny back in 1999. He didn't belive in Y2K and wore this sweater to our New Years party that year, but that history came out in the next wash and it remains an unassuming argyle sweater. It blends in at business meetings better than a bic ballpoint.",
-				"Recommendation: Keep it", "6.5/10", "image", category1);
+				"Recommendation: Keep it", "6.5/10", "fancy2.jpg", category1);
 
 		sweater1 = reviewRepo.save(sweater1);
 		sweater2 = reviewRepo.save(sweater2);
